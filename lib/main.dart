23b98a3328cc180be81c975a9e4efe88e37e6f27
Moises Alvarenga <moises.alvarenga@outlook.com>
2024-01-07 15:57:58 -0300

    Criando rotas nomeadas e configurando fontes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'utils/app-routes.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData(
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+        canvasColor: Color.fromRGBO(255, 254, 229, 1));
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+        ),
       ),
-      home: CategoriesScreen(),
+      routes: {
+        AppRoutes.home: (ctx) => const CategoriesScreen(),
+        AppRoutes.categoriesMeals: (ctx) => CategoriesMealsScreen(),
+      },
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,9 +9,10 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Vamos Cozinhar?"),
+        title: const Text("Vamos Cozinhar?"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
